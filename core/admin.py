0f1dbd91e94f0cdb5c40069eedf566f3e1db4ca2
Aqib Khan <67043041+Aqib8860@users.noreply.github.com>
2021-04-15 09:35:28 +0530

    Delete core directory

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import *
-
-# Register your models here.
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email', 'is_admin', 'is_customer', 'is_superuser', 'subscribed']
