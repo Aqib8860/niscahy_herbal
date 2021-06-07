d623ca671d56c9c0617f8944a3154a5fe798d347
Aqib Khan <67043041+Aqib8860@users.noreply.github.com>
2021-06-07 22:56:57 +0530

    Update settings.py

--- nischay_herbal_pro/settings.py
+++ nischay_herbal_pro/settings.py
@@ -150,8 +150,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Local Server
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-"""STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+"""STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
